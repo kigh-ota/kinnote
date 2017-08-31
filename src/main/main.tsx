810d9f6aac9b0ffd671a14d7716d90e9416753f5
kaiichiro-ota <kaiichiro-ota@cybozu.co.jp>
2017-08-31 23:07:35 +0900

    start only when the hash is "#note"

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -10,17 +10,19 @@ import NoteService from './domain/model/NoteService';
 // TODO: save sort type
 
 (() => {
-    kintone.events.on('app.record.index.show', (event: any) => {
-        // reset all elements
-        const BODY_CLASS = 'body-top';
-        const body: Element = document.getElementsByClassName(BODY_CLASS)[0];
-        body.innerHTML = '';
+    if (location.hash === '#note') {
+        kintone.events.on('app.record.index.show', (event: any) => {
+            // reset all elements
+            const BODY_CLASS = 'body-top';
+            const body: Element = document.getElementsByClassName(BODY_CLASS)[0];
+            body.innerHTML = '';
 
-        const container: Element  = document.createElement('DIV');
-        body.appendChild(container);
+            const container: Element = document.createElement('DIV');
+            body.appendChild(container);
 
-        NoteService.getInstance().init().then(() => {
-            ReactDOM.render(<NoteApp/>, container);
+            NoteService.getInstance().init().then(() => {
+                ReactDOM.render(<NoteApp/>, container);
+            });
         });
-    });
+    }
 })();
